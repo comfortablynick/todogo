735b57d556562d775e32eaa372bfbb4eee893c8f
Nick Murphy <comfortablynick@gmail.com>
2019-05-16 08:19:03 -0500

    Add line and word scanners

--- main.go
+++ main.go
@@ -1,9 +1,11 @@
 package main
 
 import (
-	"os"
 	"bufio"
 	"fmt"
+	"log"
+	"os"
+	"strings"
 )
 
 func check(e error) {
@@ -13,13 +15,31 @@ func check(e error) {
 }
 
 func main() {
+	log.SetOutput(os.Stderr)
+	log.SetPrefix("DEBUG ")
+	log.SetFlags(log.Lshortfile)
+
 	filehandle, err := os.Open(os.ExpandEnv("$HOME/Dropbox/todo/todo.txt"))
 	defer filehandle.Close()
 	check(err)
+	log.Printf("Opened file: %s", filehandle.Name())
 
-	scanner := bufio.NewScanner(filehandle)
+	lines := bufio.NewScanner(filehandle)
+	lines.Split(bufio.ScanLines)
 
-	for scanner.Scan() {
-		fmt.Println(scanner.Text())
+	for lines.Scan() {
+		words := bufio.NewScanner(strings.NewReader(lines.Text()))
+		words.Split(bufio.ScanWords)
+		var b strings.Builder
+		for words.Scan() {
+			chars := []rune(words.Text())
+			if chars[0] == '@' {
+				// TODO: add color here
+				fmt.Fprintf(&b, "%s ", words.Text())
+			} else {
+				fmt.Fprintf(&b, "%s ", words.Text())
+			}
+		}
+		fmt.Println(b.String())
 	}
 }
