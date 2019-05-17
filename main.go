69de50ed8a24d19387376ecacd40d14f4cdfa4ec
Nick Murphy <comfortablynick@gmail.com>
2019-05-17 17:16:05 -0500

    Add color to output

--- main.go
+++ main.go
@@ -2,11 +2,30 @@ package main
 
 import (
 	"bufio"
+	"flag"
 	"fmt"
 	"log"
 	"os"
 	"strings"
+
+	"github.com/logrusorgru/aurora"
 )
+
+const (
+	green       uint8 = 2
+	blue        uint8 = 4
+	turquoise   uint8 = 37
+	lime        uint8 = 154
+	tan         uint8 = 179
+	hotpink     uint8 = 198
+	lightorange uint8 = 215
+)
+
+// const GREY: u8 = 246;
+// const SKYBLUE: u8 = 111;
+// const OLIVE: u8 = 113;
+var au aurora.Aurora
+var colors = flag.Bool("color", true, "enable or disable colors")
 
 func check(e error) {
 	if e != nil {
@@ -14,11 +33,15 @@ func check(e error) {
 	}
 }
 
-func main() {
+func init() {
 	log.SetOutput(os.Stderr)
 	log.SetPrefix("DEBUG ")
 	log.SetFlags(log.Lshortfile)
 
+	au = aurora.NewAurora(*colors)
+}
+
+func main() {
 	filehandle, err := os.Open(os.ExpandEnv("$HOME/Dropbox/todo/todo.txt"))
 	defer filehandle.Close()
 	check(err)
@@ -33,10 +56,12 @@ func main() {
 		var b strings.Builder
 		for words.Scan() {
 			chars := []rune(words.Text())
-			if chars[0] == '@' {
-				// TODO: add color here
-				fmt.Fprintf(&b, "%s ", words.Text())
-			} else {
+			switch chars[0] {
+			case '@':
+				fmt.Fprintf(&b, "%s ", au.Index(lightorange, words.Text()))
+			case '+':
+				fmt.Fprintf(&b, "%s ", au.Index(lime, words.Text()))
+			default:
 				fmt.Fprintf(&b, "%s ", words.Text())
 			}
 		}
